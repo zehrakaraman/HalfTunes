beea30db08675c17b2353507e121f503936bf274
Zehra Karaman <zehrakaramaan@gmail.com>
2022-10-18 12:54:28 +0300

    Fix track cell constraints

--- HalfTunes/SearchView/Cell/TrackCell.swift
+++ HalfTunes/SearchView/Cell/TrackCell.swift
@@ -13,9 +13,9 @@ class TrackCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
-    @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var progressLabel: UILabel!
     
